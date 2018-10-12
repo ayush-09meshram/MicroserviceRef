ee1abceafff894d1cee9be168d4e45653e04d3e6
ayush.meshram <ayush.meshram@cgi.com>
2018-10-12 09:49:44 +0530

    Project Structure

---
+++ movieservice-config-service/src/main/java/com/stackroute/movieserviceconfigservice/MovieserviceConfigServiceApplication.java
@@ -0,0 +1,14 @@
+package com.stackroute.movieserviceconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class MovieserviceConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieserviceConfigServiceApplication.class, args);
+	}
+}
